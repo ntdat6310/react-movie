87875a74cf86a00221a34a56767ff9f601bf30b7
Nguyễn Thành Đạt <thanhdat9096@gmail.com>
2024-05-09 08:34:13 +0700

    init project

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from 'tailwindcss'
+import path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  css: {
+    // Fix Tailwind not working
+    postcss: {
+      plugins: [tailwindcss()]
+    },
+    // Enable sourcemaps
+    devSourcemap: true
+  },
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, './src')
+    }
+  }
+})
